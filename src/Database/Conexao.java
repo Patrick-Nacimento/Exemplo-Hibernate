e1a76d4bd8b6e056ac08862f31abd351626ce483
Patrick-Nacimento <patricknascimento198@gmail.com>
2018-10-10 11:23:37 -0300

    Initial commit

---
+++ src/Database/Conexao.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Database;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+/**
+ *
+ * @author Patrick
+ */
+public class Conexao {
+    
+    private StandardServiceRegistry sr = null;
+    private SessionFactory factory = null;
+    public Transaction transaction = null;
+    private Session session = null;
+    
+    public boolean conectar(){
+    
+    sr = new StandardServiceRegistryBuilder()
+            .configure("hibernate.cfg.xml").build();
+    factory = new Configuration().buildSessionFactory(sr);
+    try{
+    session = factory.openSession();
+    transaction = session.getTransaction();
+    transaction.begin();
+    return true;
+} catch (HibernateException e) {
+    e.printStackTrace();
+    if (transaction.isActive()) {
+        transaction.rollback();
+    }
+    return false;
+}
+
+}
+}
+
+
---
+++ src/Database/Estrutura.sql
@@ -0,0 +1,29 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Patrick
+ * Created: 10/10/2018
+ */
+
+DROP DATABASE IF EXISTS exemplo_hibernate_00;
+CREATE DATABASE exemplo_hibernate_00;
+USE exemplo_hibernate_00; 
+
+CREATE TABLE sistemas (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(150)
+);
+
+CREATE TABLE alunos(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(150),
+    nota1 DOUBLE,
+    nota2 DOUBLE,
+    nota3 DOUBLE,
+    nota4 DOUBLE,
+    quantidade_faltas INT
+);
+
